7be43eb0a50c8d3c3a69d4694a65aceb6ca573cd
Erik Vullings <erik.vullings@gmail.com>
2017-02-02 22:13:51 +0100

    Fix quotes

--- src/example/server.ts
+++ src/example/server.ts
@@ -1,14 +1,14 @@
 // See also https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens
-import * as express from "express";
-import { Application, Request } from "express";
-import * as bodyParser from "body-parser";
-import * as morgan from "morgan";
-import * as mongoose from "mongoose";
-import { IUser } from "../lib/models/user";
-import { CRUD } from "../lib/models/crud";
-import { nodeAuth } from "../lib/index";
+import * as express from 'express';
+import { Application, Request } from 'express';
+import * as bodyParser from 'body-parser';
+import * as morgan from 'morgan';
+import * as mongoose from 'mongoose';
+import { IUser } from '../lib/models/user';
+import { CRUD } from '../lib/models/crud';
+import { nodeAuth } from '../lib/index';
 
-const config = require("config"); // get our config file
+const config = require('config'); // get our config file
 export const server: Application = express();
 
 // =======================
@@ -21,18 +21,18 @@ mongoose.connect(config.database); // connect to database
 // use body parser so we can get info from POST and/or URL parameters
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
-server.use(bodyParser.json({ type: "application/json"}));
+server.use(bodyParser.json({ type: 'application/json'}));
 
 // use morgan to log requests to the console, but don't show the log when it is test
-if (config.util.getEnv("NODE_ENV") !== "test") {
-  server.use(morgan("dev"));
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+  server.use(morgan('dev'));
 }
 
 ////////////////////////
 // routes for testing //
 ////////////////////////
-server.get("/", (req, res) => {
-  res.send("Hello! The API is at http://localhost:" + port + "/api");
+server.get('/', (req, res) => {
+  res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
 server.use(nodeAuth(server, {
@@ -44,7 +44,7 @@ server.use(nodeAuth(server, {
   }
 }));
 
-server.route("/unprotected/resource")
+server.route('/unprotected/resource')
   .all((req, res, next) => {
     next();
   })
@@ -52,7 +52,7 @@ server.route("/unprotected/resource")
     res.json({ success: true });
   });
 
-server.get("/protected/resource", (req, res, next) => {
+server.get('/protected/resource', (req, res, next) => {
   res.status(HTTPStatusCodes.BAD_REQUEST).json({ success: false });
 });
 
@@ -60,5 +60,5 @@ server.get("/protected/resource", (req, res, next) => {
 // start the server ======
 // =======================
 server.listen(port);
-console.log("Magic happens at http://localhost:" + port);
+console.log('Magic happens at http://localhost:' + port);
 // module.exports = app; // for testing
